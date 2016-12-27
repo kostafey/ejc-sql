6a7e681a3d89e37b3d137e3a276fa3aea9b3e1c0
kostafey <kostafey@gmail.com>
2016-12-28 00:22:30 +0300

    Add ejc-test:run-sql function.

--- test/ejc-tests.el
+++ test/ejc-tests.el
@@ -59,6 +59,22 @@
          dpath)
      (file-truename "~/tmp"))))
 
+(cl-defun ejc-test:run-sql (sql &optional connect)
+  (progn
+    ;; Type SQL query and eval it.
+    (with-current-buffer (ejc-switch-to-sql-editor-buffer)
+      ;; Connect to test database, if bufer just created
+      (if connect
+          (ejc-connect connect))
+      (end-of-buffer)
+      (insert sql)
+      (ejc-eval-user-sql-at-point))
+    ;; Wait for query evaluation.
+    (sleep-for 15)
+    ;; Get the results.
+    (with-current-buffer ejc-results-buffer
+      (buffer-substring (point-max) (point-min)))))
+
 (ert-deftest ejc-test:get-connection ()
   :tags '(el+cl)
   (let* ((db-path (concat "file://" (ejc-test:get-temp-path)
@@ -92,78 +108,49 @@
               (let ((path-to-x (expand-file-name x (ejc-test:get-temp-path))))
                 (if (file-exists-p path-to-x) (delete-file path-to-x))))
             '("database.mv.db" "database.trace.db"))
+    ;; Ensure temp SQL editor buffer is empty.
+    (kill-buffer (ejc-create-sql-editor-buffer))
     ;; ------------
     ;; Create table
     (should
      (equal
       "Records affected: 0"
-      (progn
-        ;; Ensure temp SQL editor buffer is empty.
-        (kill-buffer (ejc-create-sql-editor-buffer))
-        ;; Connect to test database, type SQL query and eval it.
-        (with-current-buffer (ejc-create-sql-editor-buffer)
-          (ejc-connect "H2-test-connection")
-          (insert
-           (concat
-            "CREATE TABLE IF NOT EXISTS users (           \n"
-            "  id int(11) NOT NULL AUTO_INCREMENT,        \n"
-            "  login varchar(45),                         \n"
-            "  email varchar(45),                         \n"
-            "  first_name varchar(255) NOT NULL,          \n"
-            "  last_name varchar(45) NOT NULL,            \n"
-            "  register_date datetime DEFAULT NULL,       \n"
-            "  PRIMARY KEY (id),                          \n"
-            "  UNIQUE KEY id_UNIQUE (id)                  \n"
-            ")                                            \n"))
-          (ejc-eval-user-sql-at-point))
-        ;; Wait for query evaluation.
-        (sleep-for 15)
-        ;; Get the results.
-        (with-current-buffer ejc-results-buffer
-          (buffer-substring (point-max) (point-min))))))
+      (ejc-test:run-sql
+       (concat
+        "CREATE TABLE IF NOT EXISTS users (           \n"
+        "  id int(11) NOT NULL AUTO_INCREMENT,        \n"
+        "  login varchar(45),                         \n"
+        "  email varchar(45),                         \n"
+        "  first_name varchar(255) NOT NULL,          \n"
+        "  last_name varchar(45) NOT NULL,            \n"
+        "  register_date datetime DEFAULT NULL,       \n"
+        "  PRIMARY KEY (id),                          \n"
+        "  UNIQUE KEY id_UNIQUE (id)                  \n"
+        ")                                            \n")
+       "H2-test-connection")))
     ;; -----------
     ;; Insert data
     (should
      (equal
       "Records affected: 1"
-      (progn
-        ;; Type SQL query and eval it.
-        (with-current-buffer (ejc-switch-to-sql-editor-buffer)
-          (end-of-buffer)
-          (insert
-           (concat
-            "/                                                       \n"
-            "INSERT INTO users (login, email, first_name, last_name) \n"
-            "VALUES ('admin', 'admin@mail.com', 'John', 'Doe');      \n"
-            "INSERT INTO users (login, email, first_name, last_name) \n"
-            "VALUES ('neo', 'neo@mail.com', 'Thomas', 'Anderson');   \n"
-            "INSERT INTO users (login, email, first_name, last_name) \n"
-            "VALUES ('morpheus', 'morpheus@mail.com', 'Nil', 'Nil'); \n"))
-          (ejc-eval-user-sql-at-point))
-        ;; Wait for query evaluation.
-        (sleep-for 15)
-        ;; Get the results.
-        (with-current-buffer ejc-results-buffer
-          (buffer-substring (point-max) (point-min))))))
+      (ejc-test:run-sql
+       (concat
+        "/                                                       \n"
+        "INSERT INTO users (login, email, first_name, last_name) \n"
+        "VALUES ('admin', 'admin@mail.com', 'John', 'Doe');      \n"
+        "INSERT INTO users (login, email, first_name, last_name) \n"
+        "VALUES ('neo', 'neo@mail.com', 'Thomas', 'Anderson');   \n"
+        "INSERT INTO users (login, email, first_name, last_name) \n"
+        "VALUES ('morpheus', 'morpheus@mail.com', 'Nil', 'Nil'); \n"))))
     ;; -----------
     ;; Update data
     (should
      (equal
       "Records affected: 3"
-      (progn
-        ;; Type SQL query and eval it.
-        (with-current-buffer (ejc-switch-to-sql-editor-buffer)
-          (end-of-buffer)
-          (insert
-           (concat
-            "/                                                       \n"
-            "UPDATE users SET register_date = '2012-12-14 17:25:03'; \n"))
-          (ejc-eval-user-sql-at-point))
-        ;; Wait for query evaluation.
-        (sleep-for 15)
-        ;; Get the results.
-        (with-current-buffer ejc-results-buffer
-          (buffer-substring (point-max) (point-min))))))
+      (ejc-test:run-sql
+       (concat
+        "/                                                       \n"
+        "UPDATE users SET register_date = '2012-12-14 17:25:03'; \n"))))
     ;; -----------
     ;; Select data
     (should
@@ -174,20 +161,10 @@
        "1   admin     admin@mail.com     John        Doe        2012-12-14 17:25:03.0  \n"
        "2   neo       neo@mail.com       Thomas      Anderson   2012-12-14 17:25:03.0  \n"
        "3   morpheus  morpheus@mail.com  Nil         Nil        2012-12-14 17:25:03.0  \n")
-      (progn
-        ;; Type SQL query and eval it.
-        (with-current-buffer (ejc-switch-to-sql-editor-buffer)
-          (end-of-buffer)
-          (insert
-           (concat
-            "/                                                       \n"
-            "SELECT * FROM users;                                    \n"))
-          (ejc-eval-user-sql-at-point))
-        ;; Wait for query evaluation.
-        (sleep-for 15)
-        ;; Get the results.
-        (with-current-buffer ejc-results-buffer
-          (buffer-substring (point-max) (point-min))))))))
+      (ejc-test:run-sql
+       (concat
+        "/                                                       \n"
+        "SELECT * FROM users;                                    \n"))))))
 
 (ejc-test:run-maven-dependency-plugin)
 
