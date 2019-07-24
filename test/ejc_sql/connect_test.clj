f1905ecac28e29b93583bea54f73f7c4f546664e
kostafey@gmail.com <kostafey@gmail.com>
2019-07-24 14:09:34 +0300

    Fix tests run

--- test/ejc_sql/connect_test.clj
+++ test/ejc_sql/connect_test.clj
@@ -1,5 +1,6 @@
 (ns ejc-sql.connect-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.string :as s]
+            [clojure.test :refer :all]
             [ejc-sql.connect :refer :all]
             [ejc-sql.lib :refer :all]))
 
@@ -22,7 +23,7 @@ select * from table""")
 select * from urls where id=1""")
             (str """ -- http://localhost:8080
 select * from urls"""))
-           (seq (.split
+           (seq (s/split
                  (str """
 select * from table;
 select * from urls where id=1; -- http://localhost:8080
@@ -36,7 +37,7 @@ select * from table; """)
 select * from urls where id=1; -- http://localhost:8080
 select * from urls;"""))
            (seq
-            (.split
+            (s/split
              (str """
 select * from table; /
 select * from urls where id=1; -- http://localhost:8080
@@ -46,35 +47,35 @@ select * from urls;""")
     (is (= (list
             (str "  -- http://localhost:8080\n"
                  "select * from urls"))
-           (seq (.split (str "  -- http://localhost:8080\n"
+           (seq (s/split (str "  -- http://localhost:8080\n"
                              "select * from urls")
                         (get-separator-re "/")))))
     (is (= (list
             (str "-- http://localhost:8080\n"
                  "select * from urls"))
-           (seq (.split (str "-- http://localhost:8080\n"
+           (seq (s/split (str "-- http://localhost:8080\n"
                              "select * from urls")
                         (get-separator-re "/")))))
     (is (= '("USE testdb; "
              "CREATE TABLE customer (id INT);")
-           (seq (.split "USE testdb; /CREATE TABLE customer (id INT);"
+           (seq (s/split "USE testdb; /CREATE TABLE customer (id INT);"
                         (get-separator-re "/")))))
     (is (= '("USE testdb "
              "CREATE TABLE customer (id INT)")
-           (seq (.split "USE testdb ;CREATE TABLE customer (id INT);"
+           (seq (s/split "USE testdb ;CREATE TABLE customer (id INT);"
                         (get-separator-re ";")))))
     (is (= '("SELECT * FROM some_table "
              "SELECT * FROM other_table")
-           (seq (.split "SELECT * FROM some_table ;SELECT * FROM other_table"
+           (seq (s/split "SELECT * FROM some_table ;SELECT * FROM other_table"
                         (get-separator-re ";")))))
     (is (= '("SELECT * FROM some_table "
              "SELECT * FROM other_table")
-           (seq (.split "SELECT * FROM some_table /SELECT * FROM other_table"
+           (seq (s/split "SELECT * FROM some_table /SELECT * FROM other_table"
                         (get-separator-re "/")))))
     (is (= '("SELECT * FROM urls WHERE path like '%http://localhost%'")
-           (seq (.split (str "SELECT * FROM urls "
+           (seq (s/split (str "SELECT * FROM urls "
                              "WHERE path like '%http://localhost%'")
                         (get-separator-re "/")))))
     (is (= '("text1" "'te/xt2'" "\"te/xt3\"" "text4")
-           (seq (.split "text1/'te/xt2'/\"te/xt3\"/text4"
+           (seq (s/split "text1/'te/xt2'/\"te/xt3\"/text4"
                         (get-separator-re "/")))))))
